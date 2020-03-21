ee5d1c52fd9ca1692e9af977e09ee0769c6fec75
Paulo Köch <paulo.koch@gmail.com>
2020-03-21 18:55:31 +0000

    Use a repo URL when cloning

--- README.sh
+++ README.sh
@@ -11,5 +11,5 @@ echo $files | xargs rm -rf
 mkdir bin_install
 export PATH="$PATH:$HOME/bin_install"
 curl -fLo ~/bin_install/yadm https://github.com/TheLocehiliosan/yadm/raw/master/yadm && chmod a+x ~/bin_install/yadm
-yadm clone --bootstrap https://raw.githubusercontent.com/pkoch/yadm/master/README.sh
+yadm clone --bootstrap https://github.com/pkoch/yadm.git
 rm -rf bin_install
