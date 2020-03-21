a42a3be72042ec145e4f78ca495c77834d16e637
Paulo Köch <paulo.koch@gmail.com>
2020-03-21 18:58:27 +0000

    Less indirection on arguments

--- README.sh
+++ README.sh
@@ -3,11 +3,12 @@
 # ====
 # ssh target 'curl https://github.com/pkoch/yadm/blob/master/README.sh | bash'
 set -xeuo pipefail
-
 cd
+
 files="$(find . -mindepth 1)"
-echo $files | xargs tar czf old_contents.tgz
-echo $files | xargs rm -rf
+tar czf old_contents.tgz $files
+rm -rf $files
+
 mkdir bin_install
 export PATH="$PATH:$HOME/bin_install"
 curl -fLo ~/bin_install/yadm https://github.com/TheLocehiliosan/yadm/raw/master/yadm && chmod a+x ~/bin_install/yadm
