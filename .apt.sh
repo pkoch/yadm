386ececce78ef0b5b7d17398b34b5122967f5446
Paulo Köch <paulo.koch@gmail.com>
2020-03-26 02:39:40 +0000

    Introduce .apt.sh

---
+++ .apt.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+sudo apt update
+
+sudo apt install \
+  git \
+  git-crypt \
+  vim \
+  htop \
+  ripgrep \
+  tree \
+  curl \
+;
+
+# asdf Python
+sudo apt install \
+  build-essential  \
+  curl \
+  git \
+  libbz2-dev \
+  libffi-dev \
+  liblzma-dev \
+  libncurses5-dev \
+  libncursesw5-dev \
+  libreadline-dev \
+  libsqlite3-dev \
+  libssl-dev \
+  llvm \
+  make \
+  python-openssl \
+  tk-dev \
+  wget \
+  xz-utils \
+  zlib1g-dev \
+;
+# chrome-gnome-shell
