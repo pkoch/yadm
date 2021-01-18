bd1f68d3ecf3e078b1ef3e46a757967fa8559ff0
Paulo Köch <paulo.koch@gmail.com>
2021-01-18 13:15:28 +0000

    More yadm modernization

--- README.sh
+++ README.sh
@@ -3,7 +3,7 @@
 # ====
 # * Setup gpg key (gpg -d ~/Dropbox/gpg_keys.asc | gpg --import)
 # * Setup ssh key (mkdir -p ~/.ssh; gpg -d ~/Dropbox/id_rsa.asc > .ssh/id_rsa && chmod 400 .ssh/id_rsa)
-# * Setup keyboard (Caps, Compose) 
+# * Setup keyboard (Caps, Compose)
 # * Setup touchpad scroll direction to natural
 # * sudo ubuntu-drivers install
 # * Instal
@@ -19,5 +19,5 @@ cd
 mkdir bin_install
 export PATH="$PATH:$HOME/bin_install"
 wget -qO ~/bin_install/yadm https://github.com/TheLocehiliosan/yadm/raw/master/yadm && chmod a+x ~/bin_install/yadm
-yadm clone --bootstrap https://github.com/pkoch/yadm.git # This runs .config/yadm/bootstrap
+yadm clone --bootstrap https://github.com/pkoch/yadm.git # This runs .local/share/yadm/bootstrap
 rm -rf bin_install
