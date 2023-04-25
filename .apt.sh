9426e31f7501dbe3d8ba324540d8c8bf7b67bc73
Paulo Köch <paulo.koch@gmail.com>
2023-04-25 23:25:53 +0100

    Update adding gh repo

--- .apt.sh
+++ .apt.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key C99B11DEB97541F0
-sudo apt-add-repository https://cli.github.com/packages
-
 sudo apt update
 
 sudo apt install -y \
@@ -14,3 +11,9 @@ sudo apt install -y \
   tree \
   vim \
 ;
+
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
+&& sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
+&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+&& sudo apt update \
+&& sudo apt install gh -y
